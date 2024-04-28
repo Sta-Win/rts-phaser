b59c7a29aef519a7df49e5adf4012162b79c61da
maxime.chevallierpichon <maxime.chevallierpichon@gmail.com>
2024-04-28 21:58:56 +0200

    initial setup

---
+++ src/scenes/MainMenu.ts
@@ -0,0 +1,32 @@
+import { Scene, GameObjects } from 'phaser';
+
+export class MainMenu extends Scene
+{
+    title: GameObjects.Text;
+
+    constructor ()
+    {
+        super('MainMenu');
+    }
+
+    create ()
+    {
+        this.title = this.add.text(
+            parseInt(this.game.config.width+'')/2,
+            parseInt(this.game.config.height+'')/2,
+            'RTS Phaser',
+            {
+                fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
+                stroke: '#000000', strokeThickness: 8,
+                align: 'center'
+            }
+        ).setOrigin()
+
+
+        this.input.once('pointerdown', () => {
+
+            this.scene.start('Game');
+
+        });
+    }
+}
